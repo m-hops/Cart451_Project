074e12486082d72f3691fd1d0a0d07c70d3ce9e3
M Hops <70164577+m-hops@users.noreply.github.com>
2023-11-04 01:23:38 -0400

    Update

--- Prototype/V1/server.js
+++ Prototype/V1/server.js
@@ -1,11 +1,53 @@
+//TEST VARIABLES//
+const responseLimiter = "Limit response to one sentence and do not include personalize greetings or names"
+const worldSetup = "Pretend you are someone helping me write a fantasy novel"
+const characterGen = "Create a character"
+
+//ENV ACCESS//
+import 'dotenv/config'
+
+//EXPRESS SETUP//
+import express from 'express'; 
+import path from 'path';
+const port = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
 //OPENAI SETUP//
 import OpenAI from "openai";
-
 const openai = new OpenAI({
-    apiKey: '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',
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
-const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
-    model: "gpt-3.5-turbo",
-});
+//CALL FUNCTION
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {"role": "system", "content": responseLimiter},
+            {"role": "system", "content": worldSetup},
+            {"role": "user", "content": characterGen},
+        ],
+        max_tokens: 256,
+        temperature:1.1,
+    });
+
+    const output = JSON.stringify([chatCompletion.choices[0].message.content])
+    const regex = /[\[\]"]/g;
+    const modifiedOutput = output.replace(regex, '');
+
+    console.log("");
+    console.log(modifiedOutput);
+    console.log("");
+}
+
+//main();
